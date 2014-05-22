c37ab6ae7e3d3fab4f9d5fbae8a30f36f9c6bf44
Ji Yonggang <jungleji@gmail.com>
2014-05-22 15:38:38 +0800

    Update transform_color_format and omx_vdec from Cubie2 sdk

--- vdec/transform_color_format.h
+++ vdec/transform_color_format.h
@@ -8,9 +8,11 @@
 extern "C" {
 #endif
 
+#define ADAPT_A10_GPU_RENDER (1)
+
 extern unsigned int cedarv_address_vir2phy(void *addr);
 extern unsigned int cedarv_address_phy2vir(void *addr);
-void TransformToYUVPlaner(cedarv_picture_t* pict, void* ybuf);
+void TransformToYUVPlaner(cedarv_picture_t* pict, void* ybuf,int m_UseGPUbufferFlag);
 
 #ifdef __cplusplus
 }
